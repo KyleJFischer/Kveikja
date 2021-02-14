2de4bfabc3bb68878a9e3edcfb661c95fd83ca8d
Kyle J. Fischer <kylefischer1995@gmail.com>
2021-02-14 00:42:58 -0500

    Final Work: Moved Project to its own command

--- kveikja/commands/project.py
+++ kveikja/commands/project.py
@@ -1,1 +1,40 @@
 import yaml
+import utils.kConfig as kConfig
+import databases.projectDatabase as projDb
+import utils.commandManager as cmdManager
+import utils.kLogger as kLogger
+#Locations for Immediate Command
+commands = {}
+keys = []
+
+def runProject(commandDetails):
+    projectName = commandDetails['argument']
+    runProjectByName(projectName)
+
+def runProjectByName(projectName):
+    projectDetails = projDb.getPathFromProjectDb(projectName)
+    if (projectDetails == None):
+        print("No Project Found: " + projectName)
+        return
+
+    ExecuteCommandFile(projectDetails['path'])
+
+def loadCommandFile(fileName):
+    global commands, keys
+    with open(fileName) as file:
+        commands = yaml.full_load(file)
+        keys = list(commands)
+
+
+#MAIN EXECUTABLE
+def ExecuteCommandFile(pathToProjectFile):
+    kLogger.printDebug('Loading Project File: ' + pathToProjectFile)
+    loadCommandFile(pathToProjectFile)
+    if kConfig.defaultKeyWord not in keys:
+        kLogger.printDebug('Couldnt Find Default KeyWord')
+        return False
+    kLogger.printDebug('Executing Commands')    
+    for command in commands[kConfig.defaultKeyWord]:
+        cmdManager.runCommands(command)
+
+    return True
--- kveikja/kveikja.py
+++ kveikja/kveikja.py
@@ -2,58 +2,19 @@ import yaml
 import os
 import subprocess
 import sys
+import commands.project as projectCommand
 import utils.kConfig as kConfig
-import databases.applicationDatabase as appDb
-import databases.projectDatabase as projDb
-import utils.commandManager as cmdManager
 import utils.kLogger as kLogger
-
-#Locations for Immediate Command
-commands = {}
-keys = []
-
 
 #Loading
 def init():
     kConfig.init()
 
-def loadCommandFile(fileName):
-    global commands, keys
-    with open(fileName) as file:
-        commands = yaml.full_load(file)
-        keys = list(commands)
-
-def runProject(commandDetails):
-    projectName = commandDetails['argument']
-    runProjectByName(projectName)
-
-def runProjectByName(projectName):
-    projectDetails = projDb.getPathFromProjectDb(projectName)
-    if (projectDetails == None):
-        print("No Project Found: " + projectName)
-        return
-
-    ExecuteCommandFile(projectDetails['path'])
-
-#MAIN EXECUTABLE
-def ExecuteCommandFile(pathToProjectFile):
-    kLogger.printDebug('Loading Project File: ' + pathToProjectFile)
-    loadCommandFile(pathToProjectFile)
-    if kConfig.defaultKeyWord not in keys:
-        kLogger.printDebug('Couldnt Find Default KeyWord')
-        return False
-    kLogger.printDebug('Executing Commands')    
-    for command in commands[kConfig.defaultKeyWord]:
-        cmdManager.runCommands(command)
-
-    return True
-
-
 if __name__ == "__main__":
     init()
     argumentCount = len(sys.argv)
-    kLogger.printDebug('Arguement Count: ' + str(argumentCount))
+    kLogger.printDebug('Argument Count: ' + str(argumentCount))
     if (argumentCount == 1):
-        ExecuteCommandFile(kConfig.projectFileName)
+        projectCommand.ExecuteCommandFile(kConfig.projectFileName)
     if (argumentCount == 2):
-        runProjectByName(sys.argv[1])
+        projectCommand.runProjectByName(sys.argv[1])
